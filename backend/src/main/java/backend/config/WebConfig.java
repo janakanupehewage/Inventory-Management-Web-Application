457d5d2f41ba1484e4a5406253a7235ac47a0573
janakanupehewage <janakanupehewage02@gmail.com>
2025-03-27 23:19:27 +0530

    backend changes

--- backend/src/main/java/backend/config/WebConfig.java
+++ backend/src/main/java/backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow CORS for all endpoints
-                .allowedOrigins("http://localhost:5173","https://inventory-management-web-application.vercel.app") // Allow requests from frontend
+                .allowedOrigins("http://localhost:5173","https://inventory-management-we-git-62d7b6-madhushans-projects-a8699fbf.vercel.app") // Allow requests from frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow these methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow credentials (cookies, authorization headers, etc.)
